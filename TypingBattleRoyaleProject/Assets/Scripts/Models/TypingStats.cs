a087ca718d1dee1a6a40dfad7b938315ca79fab2
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2026-03-09 12:57:12 -0600

    Agregando Calculo de WPM

--- TypingBattleRoyaleProject/Assets/Scripts/Models/TypingStats.cs
+++ TypingBattleRoyaleProject/Assets/Scripts/Models/TypingStats.cs
@@ -1,10 +1,24 @@
-using UnityEngine;
-
 public class TypingStats
 {
     public int Hits { get; private set; }
-    public int Erros {  get; private set; }
-    public int TotalKeystroks {  get; private set; }
+    public int Errors {  get; private set; }
+    public int TotalKeystrokes {  get; private set; }
     public float TimeElapsed {  get; private set; }
+
+    public double GetWPM()
+    {
+        if (TimeElapsed <= 0)
+            return 0;
+
+        return (Hits / 5.0) / (TimeElapsed / 60.0);
+    }
+
+    public double GetAccuracy()
+    {
+        if (TotalKeystrokes <= 0)
+            return 0;
+
+        return (Hits / (double)TotalKeystrokes) * 100.0;
+    }
 
 }
