37192266c3bf620e99bebb6c71684146b4654234
ChesKrt <164610371+ChesKrt@users.noreply.github.com>
2026-03-09 18:08:05 -0600

    Set of BattleState

---
+++ TypingBattleRoyaleProject/Assets/Features/Gameloop_and_StateMachine/BattleState.cs
@@ -0,0 +1,26 @@
+public class BattleState : IGameState
+{
+    private CastInputController _castInput;
+    private PlayerController _playerController;
+    public BattleState(CastInputController castInput, PlayerController playerController)
+    {
+        this._castInput = castInput;
+        this._playerController = playerController;
+    }
+
+    void IGameState.Enter()
+    {
+        _castInput.enabled = true;
+        _playerController.enabled = false;
+    }
+
+    void IGameState.Execute(float tick) { }
+
+    void IGameState.Update() { }
+
+    void IGameState.Exit()
+    {
+        _castInput.enabled = false;
+        _playerController.enabled = true;
+    }
+}
