457429bf59f812c15e641724af697e33134453f5
SonocJM <sonxzisulishimada@gmail.com>
2026-03-17 03:41:21 -0600

    PS-111-Implementar-calculo-de-distancia-para-interaccion

--- TypingBattleRoyaleProject/Assets/Struct/InteractionRequest.cs
+++ TypingBattleRoyaleProject/Assets/Struct/InteractionRequest.cs
@@ -5,4 +5,9 @@ public class InteractionRequest
     public Vector3 playerPos;
     public Vector3 targetPos;
     public PlayerStats playerStats;
+    
+    public static bool IsInRange(Vector3 playerPos, Vector3 targetPos, float maxRange)
+    {
+        return Vector3.Distance(playerPos, targetPos) <= maxRange;
+    }
 }
