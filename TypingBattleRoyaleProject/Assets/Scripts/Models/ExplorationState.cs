3aa5b0cc19d3be415f79197ad49588c7e0091fa5
Ophion-73 <cdmx2754@amerike.edu.mx>
2026-03-18 12:07:24 -0600

    Adding MatchSessionData to Models

---
+++ TypingBattleRoyaleProject/Assets/Scripts/Models/ExplorationState.cs
@@ -0,0 +1,25 @@
+using TMPro.Examples;
+using UnityEngine;
+
+public class ExplorationState : IGameState
+{
+    public void Enter()
+    {
+        
+    }
+
+    public void Execute(float tick)
+    {
+        
+    }
+
+    public void Update()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+}
