3071b6ce83d6c4550064f015884eb756d3516af8
ChesKrt <164610371+ChesKrt@users.noreply.github.com>
2026-03-23 17:51:02 -0600

    Generation done. Debug ID

--- TypingBattleRoyaleProject/Assets/Features/Network/NetworkManagerMock.cs
+++ TypingBattleRoyaleProject/Assets/Features/Network/NetworkManagerMock.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NetworkManagerMock : MonoBehaviour
 {
     public static NetworkManagerMock Instance;
+
+    public int playerAmount = 2;
+    [SerializeField] private GameObject _playerPrefab;
+    public List<PlayerStats> Players;
 
     private void Awake()
     {
@@ -21,11 +27,25 @@ public class NetworkManagerMock : MonoBehaviour
 
     void Start()
     {
-        
-    }
+        Players = new List<PlayerStats>();
 
-    void Update()
-    {
-        
+        for (int i = 0; i < playerAmount; i++)
+        {
+            GameObject tempPlayer = Instantiate(_playerPrefab, transform.position, Quaternion.identity);
+            string id = PlayerIDGenerator.GenerateID();
+            PlayerStats generatedStats = new PlayerStats(id, 100);
+            PlayerController playerController = tempPlayer.GetComponent<PlayerController>();
+            playerController.stats = generatedStats;
+            PlayerInventory generatedInventory = new PlayerInventory();
+            playerController.inventory = generatedInventory;
+
+            if (i != 0)
+            {
+                playerController.enabled = false;
+            }
+
+            Players.Add(generatedStats);
+        }
+
     }
 }
