fb53da313286a83e1b7c073f78902d1ed233bc70
Temps91 <jorgebd3045@gmail.com>
2026-03-25 12:10:39 -0600

    Si

--- TypingBattleRoyaleProject/Assets/Features/Environment_and_Interaction/MonolithController.cs
+++ TypingBattleRoyaleProject/Assets/Features/Environment_and_Interaction/MonolithController.cs
@@ -13,6 +13,10 @@ public class MonolithController : MonoBehaviour
     [Header("Revision monolito")]
     public List<string> idPlayers = new List<string>();
     public List<Spell> spells = new List<Spell>();
+    [Header("Datos Elementos")]
+    public List<Spell> allSpells = new List<Spell>();
+    public List<SpellData> allSpellData = new List<SpellData>();
+
 
     void Awake()
     {
@@ -24,7 +28,7 @@ public class MonolithController : MonoBehaviour
     {
         idPlayers.Add(id);
     }
-    
+
     public bool IdPlayerExist(string id)
     {
         if (!idPlayers.Contains(id))
@@ -41,15 +45,66 @@ public class MonolithController : MonoBehaviour
 
     public void RemoveSpellData(Spell spellName)
     {
-        for(int i = 0; i < spells.Count; i++)
+        for (int i = 0; i < spells.Count; i++)
         {
             spells.Remove(spellName);
         }
     }
+    Elements GetMappedElement(Elements original)
+    {
+        if (original == Elements.Wind)
+        {
+            return Elements.Thunder;
+        }
+        else if (original == Elements.Water)
+        {
+            return Elements.Water;
+        }
+        else if (original == Elements.Earth)
+        {
+            return Elements.Fire;
+        }
+        else if (original == Elements.Arcane)
+        {
+            return Elements.Thunder;
+        }
+
+        return original;
+    }
 
     public void PopulateSpells()
     {
-        
-        //To do: Agregar llenado de spells
+        spells.Clear();
+
+        int randomIndex = Random.Range(1, System.Enum.GetValues(typeof(Elements)).Length);
+        Elements randomElement = (Elements)randomIndex;
+
+        for (int i = 0; i < allSpells.Count; i++)
+        {
+            if (allSpells[i].elementType == randomElement)
+            {
+                spells.Add(allSpells[i]);
+            }
+        }
+
+        for (int i = 0; i < spells.Count; i++)
+        {
+            Spell currentSpell = spells[i];
+
+            Elements mappedElement = GetMappedElement(currentSpell.elementType);
+
+            int randomTier = Random.Range(0, System.Enum.GetValues(typeof(SpellTiers)).Length);
+            SpellTiers selectedTier = (SpellTiers)randomTier;
+
+            for (int j = 0; j < allSpellData.Count; j++)
+            {
+                SpellData data = allSpellData[j];
+
+                if (data.elementType == mappedElement && data.spellTier == selectedTier)
+                {
+                    Debug.Log("Elemento " + randomElement + "  Spell " + data.runeString + "Tier" + selectedTier);
+                }
+            }
+        }
     }
 }
