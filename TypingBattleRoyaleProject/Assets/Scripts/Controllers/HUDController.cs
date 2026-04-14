4b0be6f75444cb1c8f4e4dc38da671e56676cc2b
SonocJM <cdmx2754@amerike.edu.mx>
2026-04-13 18:07:29 -0600

    avance uno, tope con pared
    te veré en REF-05

---
+++ TypingBattleRoyaleProject/Assets/Scripts/Controllers/HUDController.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDController : MonoBehaviour
+{
+    public TextMeshProUGUI lifeText;
+    public TextMeshProUGUI killCountText;
+    public TextMeshProUGUI timerText;
+    public Slider lifeUI;
+
+
+    public void UpdateHealth()
+    {
+        
+    }
+    public void UpdateKillCount()
+    {
+        
+    }
+
+    public void UpdateTime()
+    {
+        
+    }
+}
