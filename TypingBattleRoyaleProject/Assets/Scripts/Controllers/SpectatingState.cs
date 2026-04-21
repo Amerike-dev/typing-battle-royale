cbe4afcb39cb52579e68fe7f5d2888bc4eb6d0a2
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2026-04-20 19:00:57 -0600

    Agregando SpectatingState

---
+++ TypingBattleRoyaleProject/Assets/Scripts/Controllers/SpectatingState.cs
@@ -0,0 +1,24 @@
+public class SpectatingState : GameState
+{
+
+    public SpectatingState(GameplayManager manager) : base(manager)
+    {
+        
+    }
+    public override void Enter()
+    {
+    }
+
+    public override void Update()
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+    
+    public override void Execute(float tick)
+    {
+
+    }
+}
