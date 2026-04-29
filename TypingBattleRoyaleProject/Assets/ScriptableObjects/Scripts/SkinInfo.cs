df09a3905c03b343f24da3ad07dba4ac12078472
Jorge Salas <jorsalas1120@gmail.com>
2026-04-29 02:59:38 -0600

    a falta de pruebas creo que ya esta

--- TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/SkinInfo.cs
+++ TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/SkinInfo.cs
@@ -5,11 +5,5 @@ using UnityEngine;
 public class SkinInfo : ScriptableObject
 {
     public string skinName;
-    public List<GameObject> skinsPrefab = new List<GameObject>(3);
-    [Range(0, 2)] public int variationColor = 0;
-
-    public void SetSkin()
-    {
-
-    }
+    public GameObject[] models;
 }
