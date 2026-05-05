e36b0c5f3fc6aa153875cb7d3f62cebb528483ea
Temps91 <cdmx2754@amerike.edu.mx>
2026-05-04 18:53:50 -0600

    Avance 1

--- TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/Spell.cs
+++ TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/Spell.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
 
-public enum Elements { None = 0, Fire = 1, Water = 2, Earth = 3, Wind = 4, Arcane = 5, Thunder = 6, Dark, Light}
-[CreateAssetMenu(fileName = "Spell", menuName = "Wizard Stuff/Spell")]
+public enum Elements { None = 0, Fire = 1, Water = 2, Earth = 3, Wind = 4, Arcane = 5, Thunder = 6, Dark, Light, Ice, Lava}
+public enum SpellTiers { TierOne, TierTwo, TierThree }
+public enum SpellTypes {Projectile, Movility, Summon, Buff, Debuff, Aura, AOE, Weapon};
+public enum StatusEffects {None,Slow}
+[CreateAssetMenu(fileName = "Spell", menuName = "Scriptable Objects/Spell")]
 
 public class Spell : ScriptableObject
 {
+    [Header("General Info")]
     public string spellName;
     public Elements elementType;
-
+    public string description;
+    [Header("Characteristics")]
+    public SpellTypes[] spellTypes;
+    public float damage;
+    public StatusEffects debuff;
+    public float range;
+    public float speed;
+    public int uses;
     public float duration;
+    [Header("SFX")]
+    public float particleLifeDuration;
     public bool loop = true;
     public float startSpeed=2f;
     public float startSize=1f;
