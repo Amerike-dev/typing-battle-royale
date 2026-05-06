df6700c100492399980a83036c8b32ee1cafd879
Angel Bernardino <chiv_157@hotmail.com>
2026-05-06 17:25:14 -0600

    Typing

--- TypingBattleRoyaleProject/Assets/Features/TypingCombat_and_System/SpellUIController.cs
+++ TypingBattleRoyaleProject/Assets/Features/TypingCombat_and_System/SpellUIController.cs
@@ -17,9 +17,11 @@ public class SpellUIController : MonoBehaviour
     public void UpdateDisplay(int currentIndex, bool hasError)
     {
         string originalText = inputController.spellText;
+
         if (string.IsNullOrEmpty(originalText)) return;
 
         string formattedText = "";
+
         formattedText += $"<color={GetHex(correctColor)}>";
         for (int i = 0; i < currentIndex; i++)
         {
@@ -32,17 +34,16 @@ public class SpellUIController : MonoBehaviour
             if (hasError)
                 formattedText += $"<color={GetHex(incorrectColor)}>{originalText[currentIndex]}</color>";
             else
-                formattedText += originalText[currentIndex];
-            
-            if (currentIndex + 1 < originalText.Length)
+                formattedText += originalText[currentIndex]; 
+        }
+        if (currentIndex < originalText.Length)
+        {
+            formattedText += $"<color={GetHex(pendingColor)}>";
+            for (int i = currentIndex + 1; i < originalText.Length; i++)
             {
-                formattedText += $"<color={GetHex(pendingColor)}>";
-                for (int i = currentIndex + 1; i < originalText.Length; i++)
-                {
-                    formattedText += originalText[i];
-                }
-                formattedText += "</color>";
+                formattedText += originalText[i];
             }
+            formattedText += "</color>";
         }
         displayText.text = formattedText;
     }
--- TypingBattleRoyaleProject/Assets/Scripts/Controllers/CastInputController.cs
+++ TypingBattleRoyaleProject/Assets/Scripts/Controllers/CastInputController.cs
@@ -7,17 +7,13 @@ using UnityEngine.InputSystem;
 public class CastInputController : MonoBehaviour
 {
     public event Action OnSpellCast;
-    public int incorrectInput = 0;
-    public int lastInInput = 0;
-    public string spellText;
-
 
     public TMP_InputField castSpell;
     public TextMeshProUGUI spell;
+    public string spellText;
     public int stringIndex = 0;
-    //public string CastSpell = "";
-
-    public int currentIndex = 0;
+    public int lastInInput = 0;
+    public int incorrectInput = 0;
 
     [Header("Typing Stats")]
     [SerializeField] private InputActionReference _cast;
@@ -32,138 +28,58 @@ public class CastInputController : MonoBehaviour
     [Header("ViewFeedback")]
     public SpellUIController uiController;
 
-    //Timer para usar el BackSpace y evitar errores con el index del string
-    private const float _backSpaceDelay = 0.4f;
-    private const float _backSpaceCooldown = 0.5f;
-    private float _backSpaceTimer = 0f;
-
-    //Checker para que solo se borre el color en el UI sin romper el index
-    public int _errorCount = 0; //esta era priivada
 
     private void OnEnable()
     {
         _casting = true;
         _typingStats = new TypingStats();
-        //CombatLogic.SetText(spellText);
-        _errorCount = 0;
-        //Keyboard.current.onTextInput += TextInput;
         _cast.action.started += EvaluateAccuracy;
         wordsPerMinute = 0;
         accuracy = 0;
         StartCoroutine(CountTimeElapsed());
-
-        stringIndex = 0;
-        lastInInput = 0;
         castSpell.onEndEdit.AddListener(CastText);
         spell.text = spellText;
+        stringIndex = 0;
+        lastInInput = 0;
     }
 
     private void OnDisable()
     {
         StopCoroutine(CountTimeElapsed());
-        //Keyboard.current.onTextInput -= TextInput;
         castSpell.onEndEdit.RemoveListener(CastText);
     }
 
-    private void Update()
+    public void CastText(string cast)
     {
-        //BackspaceLogic();
-        //HandleBackspace();
-        //currentIndex = CombatLogic._stringIndex;
-    }
-
-
-    /*private void BackspaceLogic()
-    {
-        var backspaceKey = Keyboard.current.backspaceKey;
-
-        if (backspaceKey.wasPressedThisFrame)
-        {
-            if (_errorCount <= 0)
-            {
-                _errorCount = 0;
-                //BackspaceBehaviour();
-                if (CombatLogic._stringIndex <= 0)
-                {
-                    CombatLogic._stringIndex = 0;
-                }
-                if (CombatLogic._stringIndex > 0)
-                {
-                    _backSpaceTimer = _backSpaceDelay;
-                    BackspaceBehaviour();
-                    //CombatLogic._stringIndex--;
-                }
-            }
-            else
-            {
-                _errorCount--;
-            }
-        }
-        else if (backspaceKey.isPressed) //Esto es para que la UI no implosione. Es un contador que se vale de Update por si se deja presionado
-        {
-            _backSpaceTimer -= Time.deltaTime;
-
-            if (_backSpaceTimer <= 0)
-            {
-                _errorCount--;
-                _backSpaceTimer = _backSpaceCooldown;
-                if(_errorCount <= 0)
-                {
-                    _errorCount = 0;
-                    BackspaceBehaviour();
-                }
-                //BackspaceBehaviour();
-            }
-
-        }
-    }
-
-    private void BackspaceBehaviour()
-    {
-        if (_errorCount > 0)
-        {
-            _errorCount--;
-            bool stillError = _errorCount > 0;
-            uiController.UpdateDisplay(CombatLogic.CurrentIndex(), stillError);
-        }
-        else if (CombatLogic.CurrentIndex() > 0)
-        {
-            incorrectInput = Mathf.Max(0, incorrectInput - 1);
-            CombatLogic.EraseChar();
-            uiController.UpdateDisplay(CombatLogic.CurrentIndex(), false);
-        }
-    }*/
-   
-    /*private void TextInput(char input)
-    {
+        stringIndex = Mathf.Min(cast.Length - 1, spellText.Length - 1);
+        stringIndex = Mathf.Max(0, stringIndex);
         _totalKeysPressed++;
-        if (input == '\n' || input == '\r') return;
+        Debug.Log("Current Text: " + cast);
 
-        bool typed = CombatLogic.ValidateCharacter(input);
+        if (cast.Length == 0) return;
 
-        if (!typed)
+        if (cast[stringIndex] == spellText[stringIndex])
         {
-            _errorCount++;
+            if (stringIndex <= lastInInput + 1)
+            {
+                lastInInput = stringIndex;
+                uiController.UpdateDisplay(lastInInput + 1, false);
+            }
+        }
+
+        if (cast[stringIndex] != spellText[stringIndex])
+        {
             incorrectInput++;
-            uiController.UpdateDisplay(CombatLogic.CurrentIndex(), true);
-            Debug.Log("Correct letter: " + CombatLogic.SpellText[CombatLogic.CurrentIndex()] + " Mistypo: " + input);
-            return;
+            Debug.Log("Tsijni");
+            uiController.UpdateDisplay(lastInInput + 1, true);
         }
 
-        _errorCount = 0;
-        uiController.UpdateDisplay(CombatLogic.CurrentIndex(), false);
-
-        if (CombatLogic.spellComplete)
+        if (cast.Length == spellText.Length)
         {
-            Debug.Log("Spell Completed");
+            Debug.Log("Spell Complete Crash");
             OnDisable();
-            return;
         }
-
-        Debug.Log("Correct. Letter: " + input);
-    }*/
-
-
+    }
 
     private void EvaluateAccuracy(InputAction.CallbackContext obj)
     {
@@ -186,38 +102,6 @@ public class CastInputController : MonoBehaviour
         {
             _timeElapsed ++;
             yield return new WaitForSeconds(1f);
-        }
-    }
-
-    public void CastText(string cast)
-    {
-        stringIndex = Mathf.Min(cast.Length-1,spellText.Length-1);
-        stringIndex = Mathf.Max(0, stringIndex);
-        _totalKeysPressed++;
-        Debug.Log("Current Text: "+ cast);
-
-        if (cast.Length == 0) return;
-
-        if (cast[stringIndex] == spellText[stringIndex])
-        {
-            if (stringIndex <= lastInInput + 1)
-            {
-                lastInInput = stringIndex;
-                uiController.UpdateDisplay(lastInInput+1, false);
-            }
-        }
-        
-        if (cast[stringIndex] != spellText[stringIndex])
-        {
-            incorrectInput++;
-            Debug.Log("Tsijni");
-            uiController.UpdateDisplay(lastInInput+1, true);
-        }
-
-        if (cast.Length == spellText.Length)
-        {
-            Debug.Log("Spell Complete Crash");
-            OnDisable();
         }
     }
 }
