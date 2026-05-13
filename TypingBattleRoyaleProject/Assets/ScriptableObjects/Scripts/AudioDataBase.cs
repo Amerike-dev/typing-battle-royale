4153095750950d5b73e04b72d74672350ac95e00
Temps91 <jorgebd3045@gmail.com>
2026-05-13 12:56:35 -0600

    Subeindo arreglos del audio manager y eso

--- TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/AudioDataBase.cs
+++ TypingBattleRoyaleProject/Assets/ScriptableObjects/Scripts/AudioDataBase.cs
@@ -1,15 +1,13 @@
-using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "AudioDataBase", menuName = "Scriptable Objects/AudioDataBase")]
 public class AudioDataBase : ScriptableObject
 {
-    [SerializeField] private List<AudioEntry> list = new List<AudioEntry>();
-    
+    [SerializeField] private List<AudioEntry> list = new();
 
-    public void GetEntry(string name)
+    public AudioEntry GetEntry(string id)
     {
-
+        return list.Find(e => e.id == id);
     }
 }
