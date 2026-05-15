d6a2ea1c308d8f41eff63bda1565c78d9dbe66df
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2026-05-14 19:10:48 -0600

    Guardando para agregar Main

--- TypingBattleRoyaleProject/Assets/Scripts/Controllers/TargetSystem.cs
+++ TypingBattleRoyaleProject/Assets/Scripts/Controllers/TargetSystem.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 //Este sistema será implementado detalladamente en TBR-003. No eliminar.
 public class TargetSystem : MonoBehaviour
 {
+    [Header("Target Setting")]
+    [SerializeField] private float _defaultMaxRadius = 30f;
+
     public Transform target;
+
+    public Transform CurrentTarget { get; private set; }
 
     void Start()
     {
@@ -19,10 +24,26 @@ public class TargetSystem : MonoBehaviour
     // Busca y selecciona al enemigo más cercano dentro de un radio específico.
     public void FindClosestTarget()
     {
+        SetTarget(FindClosestTarget(transform.position, _defaultMaxRadius));
     }
 
     // Activa o desactiva el bloqueo de cámara sobre el objetivo actual
     public void ToggleLockOn()
     {
+    }
+
+    public Transform FindClosestTarget(Vector3 from, float maxRadius)
+    {
+        return null;
+    }
+
+    public void Cycle()
+    {
+        
+    }
+
+    public void Clear()
+    {
+        SetTarget(null);
     }
 }
